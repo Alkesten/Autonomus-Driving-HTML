4a29da61d4fb5226a0d5ad5c2ec731987821ab9b
Kirsten Rauffer <kirsten.rauffer@rutgers.edu>
2015-11-28 02:17:11 +0100

    Added queue

--- js/app.js
+++ js/app.js
@@ -16,9 +16,35 @@ var main = function() {
 		}
 	});
 
+	$('.queue-add').click(function(){
+		$('.queue-add').toggleClass('active');
+		$('.queue-options').toggleClass('active');
+		$('.queue-options').toggleClass('transform');
+	});
+
+	$('.add-instruction').click(function(){
+		$('.queue-add').toggleClass('active');
+		$('.queue-options').toggleClass('active');
+		$('.queue-options').toggleClass('transform');
+		
+		var instruction = $(this).text();
+		if(instruction == 'Turn Left') {
+			$('<li><i class="fa fa-hand-o-left"></i>'+instruction+'<button class="delete-button right">x</button></li>').appendTo('.queue');
+		} else 	if(instruction == 'Turn Right') {
+			$('<li><i class="fa fa-hand-o-right"></i>'+instruction+'<button class="delete-button right">x</button></li>').appendTo('.queue');
+		} else 	if(instruction == 'Stop') {
+			$('<li><i class="fa fa-hand-grab-o"></i></i>'+instruction+'<button class="delete-button right">x</button></li>').appendTo('.queue');
+		} else if(instruction == 'Forward') {
+			$('<li><i class="fa fa-hand-o-up"></i></i>'+instruction+'<button class="delete-button right">x</button></li>').appendTo('.queue');
+		} else if(instruction == 'Backward') {
+			$('<li><i class="fa fa-hand-o-down"></i></i>'+instruction+'<button class="delete-button right">x</button></li>').appendTo('.queue');
+		} else {
+			$('<li><i class="fa fa-car"></i></i>'+instruction+'<button class="delete-button right">x</button></li>').appendTo('.queue');
+		}
+	});
+
 	$('ul.tabs li').click(function(){
 		var tabId = $(this).attr('data-tab');
-
 		$('ul.tabs li').removeClass('active');
 		$('.tab-content').removeClass('active');
 
@@ -26,23 +52,10 @@ var main = function() {
 		$("#"+tabId).addClass('active');
 	})
 
-	$( "ul.sortable" ).sortable({
-		group: 'sortable',
-		onDrop: function  ($item, container, _super) {
-			$('<button class="right button-instruction">x</button>').appendTo($item);
-			$item.addClass(test);
-		}
-	});
-
-	$( ".draggable" ).draggable({
-		connectToSortable: ".sortable",
-		helper: "clone",
-		greedy: 'true',
-		revert: "invalid"
-
-	});
-
-	$( "ul, li" ).disableSelection();
+	$('ul.queue').sortable();
 };
 
 $(document).ready(main);
+$(document).on('click', '.delete-button', function() {
+    $(this).parent().remove();   
+});
