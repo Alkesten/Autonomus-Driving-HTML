cf3fa071e0055d29fee56ac9fb4c94db11869269
Kirsten Rauffer <kirstenrauffer@gmail.com>
2016-03-10 16:06:09 +0100

    bugfix: ipaddress screen

--- autonomous-driving-app/autonomous-driving-app/MainViewController.swift
+++ autonomous-driving-app/autonomous-driving-app/MainViewController.swift
@@ -12,19 +12,27 @@ class MainViewController: UIViewController {
     @IBOutlet weak var IPAddress: UITextField!
     @IBOutlet weak var enterButton: UIButton!
     
+    var car: Car!
+    var dataSocket: DataSocket!
+    var videoSocket: VideoSocket!
+    
     @IBAction func enter(sender: AnyObject) {
-        let ipv4 = IPAddress.description
+        var ipv4 = ""
+        
+        if let ip = IPAddress.text {
+            ipv4 = ip
+        } else {
+            print("Ip Address is nil")
+        }
         
         let validIpAddressRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
         
-        if ipv4 == ""{
-             print("no input!")
-        } else if (ipv4.rangeOfString(validIpAddressRegex, options: .RegularExpressionSearch) != nil){
-            print("\(ipv4) is a valid IP address")
-            var car = Car.init(ipv4: ipv4)
-            //TODO open next Windows
+        if (ipv4.rangeOfString(validIpAddressRegex, options: .RegularExpressionSearch) != nil){
+            print("\n\n \(ipv4) is a valid IP address\n\n")
+            car = Car.init(ipv4: ipv4)
+            self.performSegueWithIdentifier("nextScreen", sender: self)
         } else {
-            print("\(ipv4) is not valid")
+            print("Ip Address \(ipv4) is not valid.")
         }
     }
     override func viewDidLoad() {
