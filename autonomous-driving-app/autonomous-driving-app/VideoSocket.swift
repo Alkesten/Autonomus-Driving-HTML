9685a7de04460b896aa95a81153b063566b0957b
Moritz Kellermann <moritz.kellermann@in.tum.de>
2016-03-10 15:56:55 +0100

    change VideoSocket

--- autonomous-driving-app/autonomous-driving-app/VideoSocket.swift
+++ autonomous-driving-app/autonomous-driving-app/VideoSocket.swift
@@ -11,11 +11,11 @@ import CocoaAsyncSocket
 
 class VideoSocket: Socket {
     
-    //var video: Video
+    var video: Video
     
     override init(car: Car, localPort: UInt16) {
         super.init(car: car, localPort: localPort)
-        //self.video = Video()
+        self.video = Video()
         setupConnection()
     }
     
@@ -34,5 +34,34 @@ class VideoSocket: Socket {
         super.sendString("VideoSocketEstablished")
     }
     
+    override func udpSocket(sock : GCDAsyncUdpSocket!, didReceiveData data : NSData!,  fromAddress address : NSData!,  withFilterContext filterContext : AnyObject!) {
+        print("Received: \(data)")
+        do {
+            let payload = try extractByte(data)
+            video.processRxVideo(payload)
+        } catch let err as NSError {
+            err.description
+        }
+    }
+    
+    func extractByte(data: NSData) throws -> [UInt8]{
+        let pointer = UnsafePointer<UInt8>(data.bytes)
+        let count = data.length
+        
+        let buffer = UnsafeBufferPointer<UInt8>(start:pointer, count:count)
+        let array = [UInt8](buffer)
+        var payload = array
+        payload.removeFirst()
+        
+        let id: UInt8 = buffer[0]
+        
+        if(id != 14){
+            print("received non video data on video port")
+            let error: ErrorType = err
+            throw error
+        }
+        
+        return payload
+    }
     
 }
